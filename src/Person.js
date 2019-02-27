dce5f21e42a5f19adecf0052f2b643016bdb62c3
Keshar <paudelkeshar2055@gmail.com>
2019-02-27 09:46:57 +0545

    StateLess component is created in Person.js file

--- src/Person.js
+++ src/Person.js
@@ -1,15 +1,24 @@
 import React, { Component } from 'react';
-class Person extends Component{
-    render(){
-        return(
-            <div>
-                <p>Name: {this.props.name}</p>
-                <p>Roll No.: {this.props.roll}</p>
-                <p>Address: {this.props.address}</p>
-                <p>Mobile No.: {this.props.mobile}</p><br/><br/>   
-            
-            </div>   
-        );
-    }
-}
+// class Person extends Component{
+    // render(){
+    //     return(
+    //         <div>
+    //             <p>Name: {this.props.name}</p>
+    //             <p>Roll No.: {this.props.roll}</p>
+    //             <p>Address: {this.props.address}</p>
+    //             <p>Mobile No.: {this.props.mobile}</p><br/><br/> 
+    //         </div>
+
+    //     );
+    // }
+
+    const Person=(props)=>(
+        <div>
+                <p>Name: {props.name}</p>
+                <p>Roll No.: {props.roll}</p>
+                <p>Address: {props.address}</p>
+                <p>Mobile No.: {props.mobile}</p><br/><br/> 
+            </div>
+    )
+// }
 export default Person;
